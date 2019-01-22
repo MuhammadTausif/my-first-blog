e72e83157d52a1407900c5fb61ec33550cd1af56
MuhammadTausif <tausifasia@gmail.com>
2019-01-23 00:24:38 +0500

    Static file loading issue resolved.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login', views.login, name='login'),
+    path('register', views.singup, name='signup'),
     path('', views.post_list, name='post_list'),
 ]
