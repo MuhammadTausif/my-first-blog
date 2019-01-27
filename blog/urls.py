e6e529f543efe68d9ee15932b0216deeb38af577
MuhammadTausif <tausifasia@gmail.com>
2019-01-27 13:23:49 +0500

    Singup/login completed.

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='post_list'),
+    path('login', views.post_list, name='post_list'),
     path('register', views.singup, name='signup'),
     path('question', views.question, name='signup'),
     path('action', views.action, name='signup'),
--- blog/views.py
+++ blog/views.py
@@ -3,33 +3,41 @@ from django.utils import timezone
 from django import template
 
 from blog.main_function import get_answer
-from blog.nlp_project import login as main_login
+from blog.nlp_project import login as main_login, singup as main_singup
 from .models import Post
 
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    temp_email = request.GET.get('email', '')
-    temp_pass = request.GET.get('pass', '')
-    print('Email: {0}'.format(temp_email))
-    print('Pass: {0}'.format(temp_pass))
-    print('Res')
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    temp_login_email = request.GET.get('email', '')
+    temp_login_pass = request.GET.get('pass', '')
     temp_userName_pass = []
-    temp_userName_pass.append(temp_email)
-    temp_userName_pass.append(temp_pass)
+    temp_userName_pass.append(temp_login_email)
+    temp_userName_pass.append(temp_login_pass)
 
     temp_response = main_login(temp_userName_pass)
     print('Response: '+temp_response)
     if(temp_response == 'login,Login OK'):
         return render(request, 'blog/question.html')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {})
 
 def login(request):
     return render(request, 'blog/login.html', {})
 
 def singup(request):
+    temp_email = request.GET.get('email', '')
+    temp_pass = request.GET.get('pass', '')
+    temp_user_pass = []
+    temp_singup_message = ''
+    if(len(temp_email) != 0 ):
+        temp_user_pass.append(temp_email)
+        temp_user_pass.append(temp_pass)
+        temp_singup_message = main_singup(temp_user_pass)
+    if(temp_singup_message == 'singup,Singup OK'):
+        print('Oh good, you are sing in')
+        return render(request, 'blog/post_list.html', {})
     return render(request, 'blog/singup.html', {})
 
 def process(request):
